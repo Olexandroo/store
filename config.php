8b0289b543620e977b0859cd6a789e69b1d1e753
Alexandro <39122357+Svin228@users.noreply.github.com>
2018-09-21 13:34:03 +0300

    config file

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('APP_PATH',__DIR__.DIRECTORY_SEPARATOR);
+define('ADD_PATH',APP_PATH.'functions\add.php');
+define('FILES_PATH',APP_PATH.'uploads\files'.DIRECTORY_SEPARATOR);
+define('JSON_PATH',FILES_PATH.'json\main.json');
