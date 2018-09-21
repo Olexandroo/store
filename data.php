4233fe6bc0ca73e55804944eb097b842533b4f70
Alexandro <39122357+Svin228@users.noreply.github.com>
2018-09-21 13:32:31 +0300

    cart and data

---
+++ data.php
@@ -0,0 +1,35 @@
+<?php
+$data = [
+    'olexandro' => md5('olexandro'),
+    'alexander' => md5('god228')
+];
+$goods = [
+    '1' => [
+        'Product name' => 'Floral Dress 1',
+        'Price' => '300',
+        'URL' => 'images/item-5.jpg'
+            ],
+    '2' => [
+        'Product name' => 'Floral Dress 2',
+        'Price' => '150',
+        'URL' => 'images/item-6.jpg'
+            ],
+    '3' => [
+        'Product name' => 'Floral Dress 3',
+        'Price' => '500',
+        'URL' => 'images/item-7.jpg'
+            ],
+    '4' => [
+        'Product name' => 'Floral Dress 4',
+        'Price' => '700',
+        'URL' => 'images/item-8.jpg'
+            ]
+];
+$check = ['Загружено', 'Ошибка'];
+$categories=[
+        '1' => 'Футболка',
+        '2' => 'Джинсы',
+        '3' => 'Кроссовки',
+        '4' => 'Пиджак',
+        '5' => 'Рубашка'
+];
